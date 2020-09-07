fc20d3d100dfabeb21ac7d4e2852567c19aa7282
Supun Lakmal <supun@dxdy.tech>
2020-09-07 16:09:06 +0530

    Login Fix

---
+++ src/Component/Login.js
@@ -0,0 +1,74 @@
+import React, { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  // update state
+  const updateInput = (e, callback) => {
+    callback(e.target.value);
+  };
+
+  // Login Btn click
+  const clickLogin = (e) => {
+    e.preventDefault();
+    setUsernameError("");
+    setPasswordError("");
+
+    // TO API
+
+    if (username === "") {
+      setUsernameError("Username can't be empty");
+    }
+
+    if (password === "") {
+      setPasswordError("Password can't be empty");
+    }
+
+    if (username !== "" && password !== "") {
+      fetch("https://reqres.in/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: username, password: password }),
+      })
+        .then((res) => res.json())
+
+        .then((result) => {
+          console.log(result);
+        })
+    }
+  };
+
+  return (
+    <form>
+      <input
+        onChange={(e) => {
+          updateInput(e, setUsername, "username");
+        }}
+      />
+      {usernameError}
+      <br />
+      <input
+        onChange={(e) => {
+          updateInput(e, setPassword, "password");
+        }}
+      />
+      {passwordError}
+      <br />
+      <button
+        onClick={(e) => {
+          clickLogin(e);
+        }}
+      >
+        Login
+      </button>
+    </form>
+  );
+}
+
+export default Login;
